013b85baf42f162d65d71361e4a4b8e2371dd809
Romain Laval <romain.laval000@gmail.com>
2022-04-01 10:29:47 +0200

    correction

--- src/app/services/song-tile-service.service.ts
+++ src/app/services/song-tile-service.service.ts
@@ -16,12 +16,12 @@ export class SongTileServiceService {
   artisteID: string = ""
   description: string = "Cet arstiste n'a pas de description !"
 
-  /** Injection du service http pour les appelles aux API
+  /** Injection du service http pour les appels aux APIs
    * 
    */
   constructor(private http:HttpClient) { }
 
-  /** Retourne une valeur aléatoire en fonction du nombre d'artiste ou de musiques trouvées
+  /** Retourne une valeur aléatoire en fonction du nombre d'artistes ou de musiques trouvés
    * 
    */
   getRandomOffset(count: number){
@@ -36,10 +36,10 @@ export class SongTileServiceService {
   }
 
 
-  /** Retourne une promise contenant le nombre d'artiste à un lieu donné
+  /** Retourne une promise contenant le nombre d'artistes à un lieu donné
    * 
    * @param city Ville où chercher l'artiste
-   * @returns Une promise contenant la réponse json avec le nombres d'artistes trouvés
+   * @returns Une promise contenant la réponse JSON avec le nombre d'artistes trouvés
    */
   getNumberOfArtists(city: string): Promise<any>{
 
@@ -57,8 +57,8 @@ export class SongTileServiceService {
   /** Retourne une promise contenant un artiste aléatoire à un lieu donné
    * 
    * @param city Ville d'où provient l'artiste
-   * @param count Nombre d'artistes provenent de la ville
-   * @returns Une promise contenant la réponse json avec l'artiste trouvés
+   * @param count Nombre d'artistes provenant de la ville
+   * @returns Une promise contenant la réponse JSON avec l'artiste trouvé
    */
   getRandomArtists(city: string, count: number): Promise<any>{
     
@@ -75,26 +75,26 @@ export class SongTileServiceService {
   }
 
 
-  /** Retourne une promise contenant le nombre de musique pour un artiste donné
+  /** Retourne une promise contenant le nombre de musiques pour un artiste donné
    * 
    * @param artist Artiste choisi
-   * @returns Une promise contenant la réponse json avec le nombres de musiques trouvées
+   * @returns Une promise contenant la réponse JSON avec le nombre de musiques trouvées
    */
   getNumberOfSongs(artist: string){
 
     //endpoint : recordings
     //query : artist
     //limit : 1
-    //Uttilisation : récuperer le nombres de musiques d'un artiste
+    //Uttilisation : récuperer le nombre de musiques d'un artiste
     return this.http.get<any>('https://musicbrainz.org/ws/2/recording/?query=artist:\"'+ artist +'\"&limit=1&fmt=json', {responseType: "json"}).toPromise();
   }
 
 
   /** Retourne une promise contenant une musique aléatoire pour un artiste donné
    * 
-   * @param artist Artist choisi
+   * @param artist Artiste choisi
    * @param count Nombre de musiques d'un artiste
-   * @returns Une promise contenant la réponse json avec la musique aléatoire
+   * @returns Une promise contenant la réponse JSON avec la musique aléatoire
    */
   getRandomSong(artist: string, count: number): Promise<any>{
     
@@ -113,30 +113,30 @@ export class SongTileServiceService {
   /** Fonction principale qui extrait les données des promises
    * 
    * @param city Ville choisie
-   * @param district Region où est situé la ville
-   * @param country Pays où est situé la ville
+   * @param district Region où est située la ville
+   * @param country Pays où est située la ville
    */
   async setArtistAndSong(city: string, district: string, country: string) {
 
     //Debug
     console.log("Before");
 
-    //Essai de récuparation d'un artiste dans la ville donnée
+    //Essai de récupération d'un artiste dans la ville donnée
     await this.getNumberOfArtists(city).then(data => this.count = data["count"]);
 
-    //Pas d'artiste dans la ville
+    //Pas d'artistes dans la ville
     if(this.count == 0){
 
-      //Essai de récuperation d'un artiste dans la region
+      //Essai de récupération d'un artiste dans la région
       await this.getNumberOfArtists(district).then(data => this.count = data["count"]);
 
-      //Pas d'artiste dans la region
+      //Pas d'artistes dans la region
       if(this.count == 0){
 
-        //Essai de récuperation d'un artiste dans le pays
+        //Essai de récupération d'un artiste dans le pays
         await this.getNumberOfArtists(country).then(data => this.count = data["count"]);
 
-        //Pas d'artiste dans le pays
+        //Pas d'artistes dans le pays
         if(this.count == 0){
 
           this.artiste = "NO ARTIST";
@@ -144,7 +144,7 @@ export class SongTileServiceService {
 
         }else{
 
-          //Recherche d'artiste dans le pays
+          //Recherche d'artistes dans le pays
           await this.getRandomArtists(country, this.count).then(data => {
             this.artiste = data["artists"][0]["name"];
             this.artisteID = data["artists"][0]["id"];
@@ -152,21 +152,21 @@ export class SongTileServiceService {
           });
           this.ville = country;
 
-          //Récuperation du nombre de musiques de l'artiste
+          //Récupération du nombre de musiques de l'artiste
           await this.getNumberOfSongs(this.artiste).then(data => this.count = data["count"]);
 
-          //Pas de musique trouvée
+          //Pas de musiques trouvées
           if(this.count == 0){
             this.titre = "NO SONG";
           }else{
 
-            //Récuperation d'une musique de l'artiste
+            //Récupération d'une musique de l'artiste
             await this.getRandomSong(this.artiste, this.count).then(data => this.titre = data["recordings"][0]["title"]);
           }
         }
       }else{
 
-        //Recherche d'artiste dans la region
+        //Recherche d'artistes dans la region
         await this.getRandomArtists(district, this.count).then(data => {
           this.artiste = data["artists"][0]["name"];
           this.artisteID = data["artists"][0]["id"];
@@ -174,15 +174,15 @@ export class SongTileServiceService {
         });
         this.ville = district;
 
-        //Récuperation du nombre de musiques de l'artiste
+        //Récupération du nombre de musiques de l'artiste
         await this.getNumberOfSongs(this.artiste).then(data => this.count = data["count"]);
 
-        //Pas de musique trouvée
+        //Pas de musiques trouvées
         if(this.count == 0){
           this.titre = "NO SONG";
         }else{
 
-          //Récuperation d'une musique de l'artiste
+          //Récupération d'une musique de l'artiste
           await this.getRandomSong(this.artiste, this.count).then(data => this.titre = data["recordings"][0]["title"]);
         }
       }
@@ -197,14 +197,14 @@ export class SongTileServiceService {
       console.log("Artist : " + this.artiste);
       this.ville = city;
 
-      //Récuperation du nombre de musiques de l'artiste
+      //Récupération du nombre de musiques de l'artiste
       await this.getNumberOfSongs(this.artiste).then(data => this.count = data["count"]);
       console.log(this.count);
       if(this.count == 0){
         this.titre = "NO SONG";
       }else{
 
-        //Récuperation d'une musique de l'artiste
+        //Récupération d'une musique de l'artiste
         await this.getRandomSong(this.artiste, this.count).then(data => this.titre = data["recordings"][0]["title"]);
       }
 
@@ -221,27 +221,27 @@ export class SongTileServiceService {
    * 
    */
   async setVideoLink(){
-    //Récuperation du lien de la musique
+    //Récupération du lien de la musique
     await this.getLinkFromArtistAndSong(this.artiste, this.titre);
 
-    //Création du lien pour l'embed youtube
+    //Création du lien pour l'embed YouTube
     this.videoLink = 'https://www.youtube.com/embed/' + this.videoId + "?&autoplay=1";
   }
 
 
-  /** Récuperation de la vidéo correspondante à la musique trouvée 
+  /** Récupération de la vidéo correspondante à la musique trouvée 
    * 
    * @param artist
    * @param song 
    */
   async getLinkFromArtistAndSong(artist: string, song: string){
 
-    //endpoint : youtube search
+    //endpoint : YouTube search
     //query : artist + song
     //type : video
     //part : snipper
-    //key : youtube token
-    //Uttilisation : rechercher la musique d'un artiste sur youtube
+    //key : YouTube token
+    //Utilisation : rechercher la musique d'un artiste sur YouTube
     await this.http.get<any>('https://www.googleapis.com/youtube/v3/search/?q="' + artist + ' ' + song + '"&type=video&part=snippet&key=' + environment.youtube.token, {responseType: "json"}).toPromise().then(data => {
       this.videoId = data["items"][0]["id"]["videoId"];
     });
